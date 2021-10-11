027dccca0405852613a5dcadbba1dffe27cec850
Kossakova <zkossakova@gmail.com>
2021-10-11 15:05:34 -0500

    Adding

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
-#create a new Flask instance
+#Set Up the Database, access the SQLite database
+#create_engine() function allows us to access and query our SQLite database file.
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+#reflect the database into classes
+Base = automap_base()
+
+#reflect the database
+Base.prepare(engine, reflect=True)
+
+#save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#create a session link from Python to our database
+session = Session(engine)
+
+#define Flask app, create a Flask application called "app."
 app = Flask(__name__)
 
-#define the starting point, also known as the root
-#forward slash inside of the app.route denotes that we want to put data at the root of routes
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+#define the welcome route using @app.route("/") code
+@app.route("/")
+
+#create a function welcome() with a return statement
+def welcome():
+    return(
+#add the precipitation, stations, tobs, and temp routes that we'll need into return statement
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''') 
+
+
+
---
+++ app1.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+#create a new Flask instance
+app = Flask(__name__)
+
+#define the starting point, also known as the root
+#forward slash inside of the app.route denotes that we want to put data at the root of routes
+@app.route('/')
+def hello_world():
+    return 'Hello world'
